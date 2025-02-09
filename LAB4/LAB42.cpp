47689e65c8dbe1c63c0bd739a0e39ff9e5ba11c3
jasonyu0705 <jasonyu0705@gmail.com>
2025-02-09 08:05:51 +0000

    asda

--- LAB4/LAB42.cpp
+++ LAB4/LAB42.cpp
@@ -11,12 +11,15 @@
 #include <fcntl.h>
 using namespace std;
 
-int main(){
-    int fd= shm_open("sharedmem",O_RDWR|O_CREAT,0777);
-    ftruncate(fd,1000*sizeof(char));
+int main2(){
+    sleep(1);
+    int fd= shm_open("sharedmem",O_RDWR,0777);
+    if (fd!=-1){
+        cout<<"sucessfully linked"<<endl;
+    }
     char *p=(char*)mmap(NULL,1000*sizeof(char),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-    //read into the mmap 
-    cin>>p;
+    //keeps checking memory
+    while(p[0] != '\0'){
+        cout<<p<<endl;
+    }
 }
-
-
