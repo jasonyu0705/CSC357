9dd31509930837494e6e6792329e0aaf0d20f1cf
jasonyu0705 <jasonyu0705@gmail.com>
2025-03-02 07:29:11 +0000

    qqweqe

--- ASSIGNMENT4/ASSN4.cpp
+++ ASSIGNMENT4/ASSN4.cpp
@@ -38,26 +38,72 @@ struct tagBITMAPINFOHEADER{
     DWORD biClrUsed; //number of colors used by the bitmap
     DWORD biClrImportant; //number of colors that are important
 };
-struct HuffmanNode{
-    int data;
-    int freq;
-    struct HuffmanNode *left, *right;
+struct HuffNode{
+    int data=NULL;
+    int freq=NULL;
+    struct HuffNode *left, *right;
 };
-// struct FreqNode{
-//     int data;
-//     int freq;
-//    // struct FreqNode *next, *prav;
-// };
-int compare_freq(const void *a, const void *b) {
-    int idx1 = *(int*)a;  // Index (color value)
-    int idx2 = *(int*)b;
 
-    // Compare frequencies
-    if (bList[idx1] != bList[idx2]) {
-        return bList[idx1] - bList[idx2];  // Sort by frequency
+HuffNode *currentList;
+
+int compare(const void *ptr1, const void *ptr2){
+    const HuffNode* n1 = *(const HuffNode **)ptr1;
+    const HuffNode* n2 = *(const HuffNode **)ptr2;
+
+    if(n1->freq < n2->freq){
+        return -1;
+    }
+    else if(n1->freq == n2->freq){
+        if(n1->data < n2->data){
+            return -1;
+        }
+    }
+    else{
+        return 1;
+    }
+}
+// Function to build a Huffman tree for a specific color
+HuffNode* buildHuffmanTree(HuffNode *huff[], int *size) {
+    // Sort non-null nodes first
+    qsort(huff, *size, sizeof(HuffNode *), compare);
+
+    // Build Huffman Tree
+    while (*size > 1) {
+        // Sort again to ensure smallest nodes are at the front
+        qsort(huff, *size, sizeof(HuffNode *), compare);
+
+        // Create a new internal node
+        HuffNode *newNode = (HuffNode *)malloc(sizeof(HuffNode));
+        newNode->freq = huff[0]->freq + huff[1]->freq;
+        newNode->data = -1;  // Internal nodes don't store color values
+        newNode->left = huff[0];
+        newNode->right = huff[1];
+
+        // Replace first two nodes with the new merged node
+        huff[0] = newNode;
+
+        // Shift array left to remove second merged node
+        for (int i = 1; i < *size - 1; i++) {
+            huff[i] = huff[i + 1];
+        }
+
+        (*size)--;  // Reduce the list size
+    }
+
+    return huff[0];  // Return root of Huffman tree
+}
+//function that traverses the tree and creates codes for the values
+void generateHuffmanCodes(HuffNode* root, string code, map<int, string> &huffmanTable) {
+    if (!root) return;
+    
+    // Leaf node: store the Huffman code
+    if (root->left == NULL && root->right == NULL) {
+        huffmanTable[root->data] = code;
     }
     
-    return idx1 - idx2;  // If frequencies are equal, sort by color value
+    // Recursively go left (add 0) and right (add 1)
+    generateHuffmanCodes(root->left, code + "0", huffmanTable);
+    generateHuffmanCodes(root->right, code + "1", huffmanTable);
 }
 
 int main(int argc, char *argv[]){
@@ -96,20 +142,19 @@ int main(int argc, char *argv[]){
     } 
 
     //creating frequency lists
-    int bList[256];
-    int gList[256];
-    int rList[256];
+    HuffNode* bList[256];
+    HuffNode* gList[256];
+    HuffNode* rList[256];
 
+    //creating size counters for freqwuency noirtes
 
     //put default values in the frequency list
     for (int i = 0; i < 256; i++) {
-        //bList[i].data = i;   
-        bList[i] = 0; 
-        //gList[i].data = i;   
-        gList[i] = 0; 
-        //rList[i].data = i;   
-        rList[i] = 0; 
+        bList[i]= 0;   
+        gList[i]= 0;   
+        rList[i]= 0;   
     }
+
     //this for lop is responsible for adding the data to the frequency list
     for (int y = 0; y < bmih.biHeight; y++) {
         for (int x = 0; x < bmih.biWidth; x++) {
@@ -118,18 +163,77 @@ int main(int argc, char *argv[]){
             BYTE bVal=dataimg[3*x+y*correctWidth];
             BYTE gVal=dataimg[3*x+y*correctWidth+1];
             BYTE rVal=dataimg[3*x+y*correctWidth+2];
-
             // add 1 to the frequency of the certain color level 
-            bList[bVal]++;
-            gList[gVal]++;
-            rList[rVal]++;
+            if(bList[bVal]==NULL){
+                bList[bVal]=new HuffNode;
+                bList[bVal]->data=bVal;
+                bList[bVal]->freq=0;
+                bList[bVal]->left = NULL;
+                bList[bVal]->right = NULL;
 
+            }
+            bList[bVal]->freq++;
+
+            if(gList[gVal]==NULL){
+                gList[gVal]=new HuffNode;
+                gList[gVal]->data=gVal;
+                gList[gVal]->freq=0;
+                gList[gVal]->left = NULL;
+                gList[gVal]->right = NULL;
+            }
+            gList[gVal]->freq++;
+
+            if(rList[rVal]==NULL){
+                rList[rVal]=new HuffNode;
+                rList[rVal]->data=rVal;
+                rList[rVal]->freq=0;
+                rList[rVal]->left = NULL;
+                rList[rVal]->right = NULL;
+            }
+            rList[rVal]->freq++;
         }
     }  
-    // sort the data
-    qsort(bList, 256, sizeof(int), compare);
-    qsort(gList, 256, sizeof(int), compare);
-    qsort(rList, 256, sizeof(int), compare);
+    // Count the number of non-null Huffman nodes
+    // Declare size counters for each list
+    int rSize = 0, gSize = 0, bSize = 0;
+    for (int i = 0; i < 256; i++) {
+        if (bList[i] != NULL) {
+            bList[bSize] = bList[i];  // Move non-null nodes to the front
+            bSize++;
+        }
+        if (gList[i] != NULL) {
+            gList[gSize] = gList[i];  // Move non-null nodes to the front
+            gSize++;
+        }
+        if (rList[i] != NULL) {
+            rList[rSize] = rList[i];  // Move non-null nodes to the front
+            rSize++;
+        }
+    }
+
+ //build the trees
+HuffNode *rRoot =buildHuffmanTree(rList, &rSize);
+HuffNode *gRoot =buildHuffmanTree(gList, &gSize);
+HuffNode *bRoot =buildHuffmanTree(bList, &bSize);
+
+//get the huffman codes from the tree
+int[] rHuffCodes, gHuffCodes, bHuffCodes;
+generateHuffmanCodes(rRoot, "", rHuffCodes);
+generateHuffmanCodes(gRoot, "", gHuffCodes);
+generateHuffmanCodes(bRoot, "", bHuffCodes);
+
+
+
+
+
+    // // sort the data
+    // currentList = bList;  // Set global pointer to the blue list
+    // qsort(bList, 256, sizeof(HuffNode*), compare);
+    // currentList = gList;  // Set global pointer to the green list
+    // qsort(gList, 256, sizeof(HuffNode*), compare);
+    // currentList = rList;  // Set global pointer to the red list
+    // qsort(rList, 256, sizeof(HuffNode*), compare);
+    // //data is sorted now we can create the huffman tree
 
     // //turn bytes into float values
     //         float bfVal=(float)bVal/255;
@@ -145,6 +249,16 @@ int main(int argc, char *argv[]){
     //         dataimg[3*x+y*correctWidth] = (BYTE)(fminf(fmaxf(bfr * 255, 0), 255));
     //         dataimg[3*x+y*correctWidth + 1] = (BYTE)(fminf(fmaxf(gfr * 255, 0), 255));
     //         dataimg[3*x+y*correctWidth + 2] = (BYTE)(fminf(fmaxf(rfr * 255, 0), 255));
+    //huffman tree
+    //already in data
+    // through loops in data array and rextracting colours
+    //after fors sort it 
+    //christian said use sort but i s8used qsort
+    //MUST USE TUPLE FOR SORT OR SOME SORT OF STRUCT
+    //take top 2 and build parent out of it
+    //sort after each iteration fo the huffman tree
+
+
 
     return 0;
 }
